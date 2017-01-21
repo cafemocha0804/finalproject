731563e3c2afa5a2bf6b4f8344e89525a46a46b1
cafemocha0804 <moka_tabata@net93-dhcp-117.sfc.keio.ac.jp>
2017-01-21 14:57:46 +0900

    完成！

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
+/*let data = [ //データは配列で書く
+  coordinate("コーデ1",
+    ["学校", "冬", "雨", "赤", "寒い"],
+    "images/001.jpg"),
+  coordinate("コーデ2",
+    ["学校", "秋", "雨"],
+    "images/002.jpg"),
+  coordinate("コーデ3",
+    ["学校", "秋", "雨"],
+    "images/003.jpg"),
+  coordinate("コーデ4",
+    ["学校", "冬", "雨"],
+    "images/004.jpg"),
+  coordinate("コーデ5",
+    ["学校", "冬", "雨", "緑", "寒い"],
+    "images/005.jpg"),
+]; */
+
+
 let data = [
   coordinate("コーデ1",
-    ["女子会", "デート",　"スカート", "冬"],
+    ["女子会", "デート", "スカート", "冬"],
     "images/001.jpg"),
   coordinate("コーデ2",
     ["学校", "女子会", "ガウチョ","モノクロ", "秋", "冬" ],
@@ -27,26 +46,26 @@ let data = [
     ["学校", "秋", "スカート", "春"],
     "images/009.jpg"),
   coordinate("コーデ10",
-    ["学校", "春", "女子会", "ワンピース",　"花柄",　"デート", ],
+    ["学校", "春", "女子会", "ワンピース", "花柄", "デート", ],
     "images/010.jpg"),
 ];
 
 
 
 function skirt(){
-  return　doSearch("スカート");
+  return doSearch("スカート");
 }
 
 function dress(){
-  return　doSearch("ワンピース");
+  return doSearch("ワンピース");
 }
 
 function shorts(){
-  return　doSearch("ショートパンツ");
+  return doSearch("ショートパンツ");
 }
 
 function school(){
-  return　doSearch("学校");
+  return doSearch("学校");
 }
 
 function reset(){
@@ -55,9 +74,7 @@ function reset(){
   location.reload();
 }
 
-
-
-function coordinate(title, tag, image) {
+function coordinate(title, tag, image) { //それぞれに名前をつける
   return {
     title: title,
     tag: tag,
@@ -65,7 +82,7 @@ function coordinate(title, tag, image) {
   };
 }
 
-function contains(tag, tagList){
+function contains(tag, tagList){ //タグが一つしかないので、スペースで区切られたタグをどうやって文字列から配列にするか
   let i = 0;
   while(i < tagList.length){
     if(tagList[i] == tag){
@@ -89,7 +106,7 @@ function li(children){
   return li;
 }
 
-function img(url){
+function img(url){ //
   let i = document.createElement("img");
   i.src = url;
   return i;
@@ -101,41 +118,60 @@ function span(text){
   return s;
 }
 
-function searchResult(title, image){
+function searchResult(title, image){ //データのタイトルとデータをもらってリスト(表示場所)に返す
   let pict = img(image);
   let t = span(title);  
   return li([pict, t])
 }
 
-function addReslut(item){
-  let list = document.querySelector("#result");
-  let result = searchResult(item.title, item.image);
-  list.appendChild(result);
-}
-
-function doSearch(tag){
+function doSearch(tag) { //複数検索するよ
+  var words = tag.split("　"); //文字列を区切って配列にするよ
   let i = 0;
-  while(i < data.length){
+  while (i < data.length) { //前から見ていくよ
     let item = data[i];
-    if(contains(tag, item.tag)){
+    console.log(words);
+    let matched = words.map(word => contains(word, item.tag))//検索条件の配列と画像タグの配列を照らし合わせてあればtrueなければfoulseを返すよ
+      .reduce((a, b) => a && b);//そのtrueとfolseを全部繋げる
+    if(matched){//全部trueだったら
       addReslut(item);
     }
-    i = i + 1;
+    /*
+  for(word of words){ //配列の中の文字列を延々検証する
+      if(!contains(word,item.tag)){ //全部含まないにすると含まないものだけが表示される
+        //delet();
+        //addReslut(item);
+        //return; ここでreturnを入れると最初に引っかかったやつだけの表示になる
+
+      }else{
+        addReslut(item);
+      }
   }
+  
+  //return; //ここに入れると何も起こらない
+  */
+  i = i + 1;
+  }
+} //「タグを含む回数」分写真が表示されてるよ 多分一単語ずつaddResultが動いてるからだよ
+
+
+function addReslut(item){ //検索結果を表示
+  let list = document.querySelector("#result");
+  let result = searchResult(item.title, item.image);　
+  list.appendChild(result); //対象リストの子要素にする
 }
 
-function clearSearchResult(){
+function clearSearchResult(){　//ここで検索前に前の検索結果をリセットする
 }
 
 function beforeSearch(){
-  clearSearchResult();
+  clearSearchResult(); //ここで検索前に前の検索結果をリセットする
 }
 
-function afterSearch(){
+function afterSearch(){ //ここで検索後の処理を行う
 }
 
-function search(){
-  let tagInput = document.querySelector("#tag");
+function search(){ //検索を行う
+  let tagInput = document.querySelector("#tag"); //HTMLの中に入力されたタグを引っ張ってくる
   let tag = tagInput.value;
   console.log(`input: ${tag}`);
   beforeSearch();
@@ -143,10 +179,9 @@ function search(){
   afterSearch();
 }
 
-function start(){
-  let button = document.querySelector("#search");
-  button.addEventListener("click", search);
+function start(){ //最初にやること
+  let button = document.querySelector("#search"); 
+  button.addEventListener("click", search); //ボタンがクリックされたら検索をはじめる
 }
-
 
 start();
